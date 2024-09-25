2304b4dd0be12d03e2bce3e81ed546d2e1c710cd
unknown <iamadelowotan@gmail.com>
2024-09-25 16:12:41 +0100

    implement: initial configurations setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p className="flex items-center justify-center text-5xl font-bold">
+        Nextgen website build
+      </p>
+    </div>
+  );
+}
