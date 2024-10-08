2ef0c9b6a37605cd233a3f2065e41efa758805c9
unknown <iamadelowotan@gmail.com>
2024-10-08 11:19:37 +0100

    contact details updated

--- src/app/(website)/contact/page.tsx
+++ src/app/(website)/contact/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ContactForm from "./ContactForm";
+import { MapPinIcon } from "lucide-react";
 
 const page = () => {
   return (
@@ -7,19 +8,32 @@ const page = () => {
       <div className="contain space-y-12">
         <div>
           <p className="text-center text-sm">How can we help you?</p>
-          <h1 className="text-orange-dark text-center text-4xl font-semibold">
+          <h1 className="text-center text-4xl font-semibold text-orange-dark">
             Get in touch
           </h1>
         </div>
         <div className="relative flex justify-center gap-8 max-md:flex-col">
-          <div className="">
+          <div className="space-y-6 rounded-md bg-white/50 pt-6 shadow-md">
+            <div className="space-y-4 px-6">
+              <h3 className="text-2xl font-semibold text-orange-dark">
+                Contact Information
+              </h3>
+              <div />
+              <p className="flex max-w-lg flex-wrap items-center gap-2">
+                <MapPinIcon className="size-4 text-orange-dark" />
+                km18 lekki-Epe expressway, Agungi, Lagos, Nigeria.
+              </p>
+              {/* <p className="flex items-center gap-2">
+                <PhoneCall className="size-4 text-orange-dark" />
+              </p> */}
+            </div>
             <Image
               src="/assets/images/contactimage.png"
               alt="Nextgen contact support"
               width={400}
               height={800}
               quality={100}
-              className="rounded-md shadow-sm"
+              className="w-full rounded-md object-cover shadow-sm"
             />
           </div>
           <ContactForm />
