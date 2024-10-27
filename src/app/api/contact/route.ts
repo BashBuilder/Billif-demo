d7ae4a3d902a3c12372839df9ba0ad789a8eeaba
unknown <iamadelowotan@gmail.com>
2024-10-27 18:15:16 +0100

    fix: working tree cleaned

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,39 +1,12 @@
 import { sendSlackMessage } from "@/lib/api-request";
-<<<<<<< HEAD
-import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const key = `user:contact`;
-    const user = await kv.hgetall(key);
-
-    if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 404 });
-    }
-    return NextResponse.json(user, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 400 });
-  }
-}
-
-=======
-import { NextRequest, NextResponse } from "next/server";
-
->>>>>>> anthony-dev
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
     await sendSlackMessage(body);
 
-<<<<<<< HEAD
-    const key = `user:contact`;
-
-    await kv.hmset(key, body);
-    await sendSlackMessage(body);
-=======
->>>>>>> anthony-dev
     return NextResponse.json(
       { message: "Data saved successfully" },
       { status: 200 },
--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,20 +1,9 @@
 import { sendSlackMessage } from "@/lib/api-request";
-<<<<<<< HEAD
-import { kv } from "@vercel/kv";
-=======
->>>>>>> anthony-dev
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-<<<<<<< HEAD
-    const key = `user:waitlist`;
-
-    await kv.hmset(key, body);
-=======
-
->>>>>>> anthony-dev
     await sendSlackMessage(body);
 
     return NextResponse.json(
