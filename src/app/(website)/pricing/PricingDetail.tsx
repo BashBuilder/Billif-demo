ecdc4e9b6d20ac5da8e4f0c46ac1ee31237d788e
unknown <iamadelowotan@gmail.com>
2025-02-10 16:49:52 +0100

    update: added pricing plan

--- src/app/(website)/pricing/PricingDetail.tsx
+++ src/app/(website)/pricing/PricingDetail.tsx
@@ -1,33 +1,28 @@
 import Demo from "@/components/global/demo";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-// const pricingData = [
-//   {
-//     title: "Essentials",
-//     price: 45,
-//     description:
-//       "Spend 50% less time on AP or automate invoicing and get paid 2x faster.",
-//   },
-//   {
-//     title: "Team",
-//     price: 55,
-//     description:
-//       "Get more granular controls and automatic 2-way sync with leading accounting software.",
-//   },
-//   {
-//     title: "Corporate",
-//     price: 79,
-//     description:
-//       "Get additional customization and confidently manage AP and AR all in one place.",
-//   },
-//   {
-//     title: "Enterprise",
-//     price: "Custom price",
-//     description:
-//       "Unlock enhanced security, premium support, multi-location accounting capabilities, and more.",
-//   },
-// ];
+const pricingData = [
+  {
+    title: "Premium",
+    price: "49,999",
+    description: [
+      "Everything in Essential Package plus",
+      "Automate 2-way sync with accounting software",
+      "QuickBooks and Xero Bespoke support",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "Custom price",
+    description: [
+      "Everything in Premium Package plus;",
+      "Custom integration and setup",
+      "Custom reconciliation",
+    ],
+  },
+];
 
 const PricingDetail = () => {
   return (
@@ -57,43 +52,91 @@ const PricingDetail = () => {
         </div>
       </div>
 
-      <div className="contain space-y-8">
+      {/* <div className="contain space-y-8">
         <h2 className="animate-pulse py-24 text-center font-heading text-4xl font-semibold text-orange-dark">
           Coming soon ...
         </h2>
-      </div>
-      {/* <div className="contain space-y-8">
-        <h2 className="font-heading text-orange-dark text-center text-3xl font-semibold">
+      </div> */}
+      <div className="contain space-y-8">
+        <h2 className="text-center font-heading text-3xl font-semibold text-orange-dark">
           Account Payable and Account Receivable
         </h2>
-        <div className="flex flex-wrap justify-center gap-6">
+
+        <div className="mx-auto grid max-w-screen-md gap-6 md:grid-cols-2">
+          <div className="col-span-2 w-full space-y-6 rounded-2xl bg-white p-6 shadow">
+            <h5 className="font-heading text-lg text-orange-dark">
+              Essentials
+            </h5>
+            <div className="flex gap-1">
+              <span>₦</span>
+              <h1 className="font-heading text-3xl text-orange-dark">39,999</h1>
+              <p className="text-xs text-black/50">user/month</p>
+            </div>
+            <div className="grid justify-between gap-2 md:grid-cols-2">
+              <div className="space-y-2">
+                <h6 className="font-heading text-sm font-semibold text-orange-dark">
+                  Payable
+                </h6>
+                <ul className="list-disc space-y-2 pl-4 text-sm text-black/70">
+                  <li>Easily enter bills</li>
+                  <li>Automate approval workflows</li>
+                  <li>Pay by Bank Transfer</li>
+                  <li>Manage bills from a centralized inbox</li>
+                  <li>Set standard approval policies</li>
+                </ul>
+              </div>
+              <div className="space-y-2">
+                <h6 className="font-heading text-sm font-semibold text-orange-dark">
+                  Receivable
+                </h6>
+                <ul className="list-disc space-y-2 pl-4 text-sm text-black/70">
+                  <li>Create professional, custom invoices</li>
+                  <li>Send invoices flexibly</li>
+                  <li>Track invoices flexibly</li>
+                  <li>Automate payment reminders</li>
+                  <li>Get paid through payment link</li>
+                  <li>Create recurring invoices</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <Link
+                href="#"
+                className="cursor-pointer rounded-full bg-primary px-4 py-2 text-xs font-semibold text-white"
+              >
+                Request a demo
+              </Link>
+            </div>
+          </div>
           {pricingData.map((item) => (
             <div
               key={item.title}
-              className="w-full space-y-6 rounded-2xl bg-white p-6 shadow-md md:max-w-64"
+              className="col-span-1 w-full space-y-6 rounded-2xl bg-white p-6 shadow"
             >
-              <h5 className="font-heading text-orange-dark text-lg">
+              <h5 className="font-heading text-lg text-orange-dark">
                 {item.title}
               </h5>
               {item.price === "Custom price" ? (
-                <h1 className="font-heading text-orange-dark text-3xl">
+                <h1 className="font-heading text-3xl text-orange-dark">
                   Custom price
                 </h1>
               ) : (
                 <div className="flex gap-1">
-                  <span>$</span>
-                  <h1 className="font-heading text-orange-dark text-6xl">
+                  <span>₦</span>
+                  <h1 className="font-heading text-3xl text-orange-dark">
                     {item.price}
                   </h1>
-                  <p className="text-xs text-black/50">
-                    user/
-                    <br />
-                    month
-                  </p>
+                  <p className="text-xs text-black/50">user/month</p>
                 </div>
               )}
 
-              <p className="text-sm text-black/70">{item.description}</p>
+              <ul className="list-disc space-y-2 pl-4 text-sm text-black/70">
+                {item.description?.map((item, index) => (
+                  <li key={index} className="text-sm text-black/70">
+                    {item}
+                  </li>
+                ))}
+              </ul>
               <div className="">
                 <Link
                   href="#"
@@ -105,7 +148,7 @@ const PricingDetail = () => {
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
     </section>
   );
 };
