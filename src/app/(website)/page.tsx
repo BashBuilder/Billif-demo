5297cbb4f0800bc15388adca12020941d022c275
unknown <iamadelowotan@gmail.com>
2025-08-23 21:41:00 +0100

    update removed initial landing from the desgin

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,19 +1,14 @@
-import GetStarted from "@/components/modules/general/GetStarted";
-import DesignedIntegration from "@/components/modules/home/DesignedIntegration";
-import Details from "@/components/modules/home/Details";
 import Hero from "@/components/modules/home/Hero";
-import PurposeBuilt from "@/components/modules/home/PurposeBuilt";
-import WhyNextgen from "@/components/modules/home/WhyNextgen";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <WhyNextgen />
+      {/* <WhyNextgen />
       <Details />
       <PurposeBuilt />
       <DesignedIntegration />
-      <GetStarted />
+      <GetStarted /> */}
     </main>
   );
 }
